87100821296dca0e0d751d769baafac3c931be16
ZAN DoYe <zandoye@gmail.com>
2022-07-06 10:37:12 +0800

    comment

--- tailcall.go
+++ tailcall.go
@@ -1,11 +1,13 @@
 package tailcall
 
+// TailRec[T] reprents a tailcall return value of which the status is done or waiting to be evaluated
 type TailRec[T any] struct {
     next func() TailRec[T]
     result T
     done bool
 }
 
+// return a TailRec[T] of which the status is done with the final value
 func TailRet[T any](r T) TailRec[T] {
     return TailRec[T] {
         result: r,
@@ -13,6 +15,7 @@ func TailRet[T any](r T) TailRec[T] {
     }
 }
 
+// start to get the final result from a TailRec[T]
 func TailStart[T any](tr TailRec[T]) T {
     for ;; {
         if tr.done {
@@ -23,6 +26,7 @@ func TailStart[T any](tr TailRec[T]) T {
     }
 }
 
+// TailCall helper to call a function whose arity is 0
 func TailCall0[T any](f func() TailRec[T]) TailRec[T] {
     return TailRec[T] {
         next: f,
@@ -30,6 +34,7 @@ func TailCall0[T any](f func() TailRec[T]) TailRec[T] {
     }
 }
 
+// the default TailCall function, elegant and still, used for connecting the next step of the computation
 func TailCall[O any, I any](f func(i I) TailRec[O], i I) TailRec[O] {
     var g= func () TailRec[O] {
         return f(i)
@@ -40,6 +45,7 @@ func TailCall[O any, I any](f func(i I) TailRec[O], i I) TailRec[O] {
     }
 }
 
+// TailCall helper to call a function whose arity is 2
 func TailCall2[
     O any, I1 any, I2 any](
     f func(i1 I1, i2 I2) TailRec[O],
@@ -53,6 +59,7 @@ func TailCall2[
     }
 }
 
+// TailCall helper to call a function whose arity is 3
 func TailCall3[
     O any, I1 any, I2 any, I3 any](
     f func(i1 I1, i2 I2, i3 I3) TailRec[O],
@@ -66,6 +73,7 @@ func TailCall3[
     }
 }
 
+// TailCall helper to call a function whose arity is 4
 func TailCall4[
     O any, I1 any, I2 any, I3 any, I4 any] (
     f func(i1 I1, i2 I2, i3 I3, i4 I4) TailRec[O],
@@ -79,6 +87,7 @@ func TailCall4[
     }
 }
 
+// TailCall helper to call a function whose arity is 5
 func TailCall5[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any] (
     f func(i1 I1, i2 I2, i3 I3, i4 I4, i5 I5) TailRec[O],
@@ -92,6 +101,7 @@ func TailCall5[
     }
 }
 
+// TailCall helper to call a function whose arity is 6
 func TailCall6[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any] (
     f func(i1 I1, i2 I2, i3 I3, i4 I4, i5 I5, i6 I6) TailRec[O],
@@ -105,6 +115,7 @@ func TailCall6[
     }
 }
 
+// TailCall helper to call a function whose arity is 7
 func TailCall7[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any] (
     f func(i1 I1, i2 I2, i3 I3, i4 I4, i5 I5, i6 I6, i7 I7) TailRec[O],
@@ -118,6 +129,7 @@ func TailCall7[
     }
 }
 
+// TailCall helper to call a function whose arity is 8
 func TailCall8[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any] (
     f func(i1 I1, i2 I2, i3 I3, i4 I4, i5 I5, i6 I6, i7 I7, i8 I8) TailRec[O],
@@ -131,6 +143,7 @@ func TailCall8[
     }
 }
 
+// TailCall helper to call a function whose arity is 9
 func TailCall9[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any] (
@@ -147,6 +160,7 @@ func TailCall9[
     }
 }
 
+// TailCall helper to call a function whose arity is 10
 func TailCall10[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any] (
@@ -163,6 +177,7 @@ func TailCall10[
     }
 }
 
+// TailCall helper to call a function whose arity is 11
 func TailCall11[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any, I11 any] (
@@ -179,6 +194,7 @@ func TailCall11[
     }
 }
 
+// TailCall helper to call a function whose arity is 12
 func TailCall12[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any, I11 any, I12 any] (
@@ -195,6 +211,7 @@ func TailCall12[
     }
 }
 
+// TailCall helper to call a function whose arity is 13
 func TailCall13[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any, I11 any, I12 any, I13 any] (
@@ -211,6 +228,7 @@ func TailCall13[
     }
 }
 
+// TailCall helper to call a function whose arity is 14
 func TailCall14[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any, I11 any, I12 any, I13 any, I14 any] (
@@ -228,6 +246,7 @@ func TailCall14[
     }
 }
 
+// TailCall helper to call a function whose arity is 15
 func TailCall15[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any, I11 any, I12 any, I13 any, I14 any, I15 any] (
@@ -245,6 +264,7 @@ func TailCall15[
     }
 }
 
+// TailCall helper to call a function whose arity is 16
 func TailCall16[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any, I11 any, I12 any, I13 any, I14 any, I15 any, I16 any] (
@@ -262,6 +282,7 @@ func TailCall16[
     }
 }
 
+// TailCall helper to call a function whose arity is 17
 func TailCall17[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any, I11 any, I12 any, I13 any, I14 any, I15 any, I16 any,
@@ -283,6 +304,7 @@ func TailCall17[
     }
 }
 
+// TailCall helper to call a function whose arity is 18
 func TailCall18[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any, I11 any, I12 any, I13 any, I14 any, I15 any, I16 any,
@@ -304,6 +326,7 @@ func TailCall18[
     }
 }
 
+// TailCall helper to call a function whose arity is 19
 func TailCall19[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any, I11 any, I12 any, I13 any, I14 any, I15 any, I16 any,
@@ -325,6 +348,7 @@ func TailCall19[
     }
 }
 
+// TailCall helper to call a function whose arity is 20
 func TailCall20[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any, I11 any, I12 any, I13 any, I14 any, I15 any, I16 any,
@@ -346,6 +370,7 @@ func TailCall20[
     }
 }
 
+// TailCall helper to call a function whose arity is 21
 func TailCall21[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any, I11 any, I12 any, I13 any, I14 any, I15 any, I16 any,
@@ -367,6 +392,7 @@ func TailCall21[
     }
 }
 
+// TailCall helper to call a function whose arity is 22
 func TailCall22[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any, I11 any, I12 any, I13 any, I14 any, I15 any, I16 any,
@@ -388,6 +414,7 @@ func TailCall22[
     }
 }
 
+// TailCall helper to call a function whose arity is 23
 func TailCall23[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any, I11 any, I12 any, I13 any, I14 any, I15 any, I16 any,
@@ -409,6 +436,7 @@ func TailCall23[
     }
 }
 
+// TailCall helper to call a function whose arity is 24
 func TailCall24[
     O any, I1 any, I2 any, I3 any, I4 any, I5 any, I6 any, I7 any, I8 any,
     I9 any, I10 any, I11 any, I12 any, I13 any, I14 any, I15 any, I16 any,
